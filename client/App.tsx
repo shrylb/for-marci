339be283d8d13ba419ab70fdd7b1c88417941013
shryl <sherylbetonio.work@gmail.com>
2025-10-20 20:03:00 +0800

    added file edits

--- client/App.tsx
+++ client/App.tsx
@@ -5,8 +5,7 @@ import { createRoot } from "react-dom/client";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import { HashRouter as Router, Routes, Route, HashRouter } from "react-router-dom";import Header from "./components/Header";
 import Index from "./pages/Index";
 import Listings from "./pages/Listings";
 import About from "./pages/About";
@@ -19,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <div className="fixed top-0 left-0 right-0 z-50">
           <Header />
         </div>
@@ -30,7 +29,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
